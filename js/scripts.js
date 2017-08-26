85102985762e9bb29f6efe4c8829ff623a422c0d
Debbie Michel <debbiemichel1633@gmail.com>
2017-08-25 18:53:47 -0700

    Output price per choices made

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,32 @@ function Option(size, topping) {
 }
 Option.prototype.price = function(){
    var total = this.sizes + this.toppings;
-
      if((this.sizes === "Small") && (this.toppings === "Olives")){
        return "$6.00";
-       console.log("$6.00");
-     }
-     else{
-       console.log('No Price available');
+     }else if((this.sizes === "Small") && (this.toppings === "Mushrooms")){
+       return "$6.50";
+     }else if((this.sizes === "Small") && (this.toppings === "Onions")){
+       return "$6.75";
+     }else if((this.sizes === "Medium") && (this.toppings === "Olives")){
+       return "$7.00";
+     }else if((this.sizes === "Medium") && (this.toppings === "Mushrooms")){
+       return "$7.50";
+     }else if((this.sizes === "Medium") && (this.toppings === "Onions")){
+       return "$7.75";
+     }else if((this.sizes === "Large") && (this.toppings === "Olives")){
+       return "$10.00";
+     }else if((this.sizes === "Large") && (this.toppings === "Mushrooms")){
+       return "$10.50";
+     }else if((this.sizes === "Large") && (this.toppings === "Onions")){
+       return "$10.75";
+     }else if((this.sizes === "Family") && (this.toppings === "Olives")){
+       return "$12.00";
+     }else if((this.sizes === "Family") && (this.toppings === "Mushrooms")){
+       return "$12.50";
+     }else if((this.sizes === "Family") && (this.toppings === "Onions")){
+       return "$12.75";
+     }else{
+       return'No Price available';
      }
      return total;
    }
@@ -20,6 +39,7 @@ Option.prototype.price = function(){
 $(document).ready(function(){
   $("form#pizza").submit(function(event){
     event.preventDefault();
+    $("#output").text("");
 
     var size = $("#size").val();
     var topping = $("#topping").val();
