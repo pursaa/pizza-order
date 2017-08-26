6e59189cf05543892a8064715b7fdafdbf5fe107
Debbie Michel <debbiemichel1633@gmail.com>
2017-08-25 19:18:57 -0700

    Hide the pizza section until the submit button is hit

--- js/scripts.js
+++ js/scripts.js
@@ -4,36 +4,36 @@ function Option(size, topping) {
   this.toppings = topping;
 }
 Option.prototype.price = function(){
-   var total = this.sizes + this.toppings;
-     if((this.sizes === "Small") && (this.toppings === "Olives")){
-       return "$6.00";
-     }else if((this.sizes === "Small") && (this.toppings === "Mushrooms")){
-       return "$6.50";
-     }else if((this.sizes === "Small") && (this.toppings === "Onions")){
-       return "$6.75";
-     }else if((this.sizes === "Medium") && (this.toppings === "Olives")){
-       return "$7.00";
-     }else if((this.sizes === "Medium") && (this.toppings === "Mushrooms")){
-       return "$7.50";
-     }else if((this.sizes === "Medium") && (this.toppings === "Onions")){
-       return "$7.75";
-     }else if((this.sizes === "Large") && (this.toppings === "Olives")){
-       return "$10.00";
-     }else if((this.sizes === "Large") && (this.toppings === "Mushrooms")){
-       return "$10.50";
-     }else if((this.sizes === "Large") && (this.toppings === "Onions")){
-       return "$10.75";
-     }else if((this.sizes === "Family") && (this.toppings === "Olives")){
-       return "$12.00";
-     }else if((this.sizes === "Family") && (this.toppings === "Mushrooms")){
-       return "$12.50";
-     }else if((this.sizes === "Family") && (this.toppings === "Onions")){
-       return "$12.75";
-     }else{
-       return'No Price available';
-     }
-     return total;
+  var total = this.sizes + this.toppings;
+   if((this.sizes === "Small") && (this.toppings === "Olives")){
+     return "$6.00";
+   }else if((this.sizes === "Small") && (this.toppings === "Mushrooms")){
+     return "$6.50";
+   }else if((this.sizes === "Small") && (this.toppings === "Onions")){
+     return "$6.75";
+   }else if((this.sizes === "Medium") && (this.toppings === "Olives")){
+     return "$7.00";
+   }else if((this.sizes === "Medium") && (this.toppings === "Mushrooms")){
+     return "$7.50";
+   }else if((this.sizes === "Medium") && (this.toppings === "Onions")){
+     return "$7.75";
+   }else if((this.sizes === "Large") && (this.toppings === "Olives")){
+     return "$10.00";
+   }else if((this.sizes === "Large") && (this.toppings === "Mushrooms")){
+     return "$10.50";
+   }else if((this.sizes === "Large") && (this.toppings === "Onions")){
+     return "$10.75";
+   }else if((this.sizes === "Family") && (this.toppings === "Olives")){
+     return "$12.00";
+   }else if((this.sizes === "Family") && (this.toppings === "Mushrooms")){
+     return "$12.50";
+   }else if((this.sizes === "Family") && (this.toppings === "Onions")){
+     return "$12.75";
+   }else{
+     return'No Price available';
    }
+ // return total;
+}
 
 //User interface logic
 $(document).ready(function(){
@@ -48,6 +48,7 @@ $(document).ready(function(){
 
     var pizzaPrice = new Option((size), (topping));
     var total = pizzaPrice.price();
+    $("#cost").show()
     $("#output").append(total);
   });
 });
